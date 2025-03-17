4a63cab16c39eadbab8fe65ca9c1b4c1621dba89
Adrian <adriansanduta@gmail.com>
2025-03-16 20:33:11 -0500

    tictactoe1.2

--- TicTacToe Starter/script.js
+++ TicTacToe Starter/script.js
@@ -3,10 +3,26 @@ const title = document.querySelector('.board__title');
 
 let currentPlayer = "X";
 
+let board = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+
 allSquares.forEach((square, i) => {
     square.addEventListener('click', () => {
         square.innerHTML = currentPlayer;
+
+        checkWin();
+
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         title.innerHTML = `${currentPlayer}'s Turn`;
     });
-});    
+});  
+
+function checkWin() {
+    const winningIndicies = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+        [0, 4, 8], [2, 4, 6] // diagonal
+    ];
+
+    for (let i = 0; i < winningIndicies.length; i++) {
+        const matchingIndicies = winningIndicies[i];
+}
